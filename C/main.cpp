f77c866fb2ef649ebed83bb1c012b34a8fe4dc0a
Maksim <lkmaksss@gmail.com>
2019-03-04 23:10:54 +0300

    comments for code in main.cpp

--- C/main.cpp
+++ C/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << sqr(2) << std::endl;
-    std::cout << cub(2) << std::endl;
-    std::cout << fou(2) << std::endl;
+    std::cout << sqr(2) << std::endl; // in index.h
+    std::cout << cub(2) << std::endl; // in lib.h
+    std::cout << fou(2) << std::endl; // in main.h
 }
